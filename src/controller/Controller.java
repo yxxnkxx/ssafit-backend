abaec8ea7798889dd7a55e8cd1e8433b48b2fd2b
Lee YunGyung <yxxnkxx@gmail.com>
2022-09-16 11:06:26 +0900

    main

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -14,12 +14,36 @@ import dao.MainDaoImpl;
 @WebServlet("/main")
 public class Controller extends HttpServlet {
 	private MainDao mainDao = MainDaoImpl.getInstance();
-	
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		process(request, response);
+
+	}
+
+	private void process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setAttribute("interestList", mainDao.selectInterestViewFitVideo());
-		request.setAttribute("partList", mainDao.selectPartfitVideo());
+		String action = request.getParameter("action");
+		if (action != null) {
+			switch (action) {
+			case "select":
+				selectPartList(request, response);
+				break;
+
+			}
+		}
 
 		request.getRequestDispatcher("/main.jsp").forward(request, response);
+
 	}
+
+	private void selectPartList(HttpServletRequest request, HttpServletResponse response) {
+		String part = request.getParameter("part");
+
+		request.setAttribute("partList", mainDao.selectPartfitVideo(part));
+	}
+
 }
--- src/dao/MainDaoImpl.java
+++ src/dao/MainDaoImpl.java
@@ -1,6 +1,8 @@
 package dao;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import dto.Video;
@@ -14,10 +16,10 @@ public class MainDaoImpl implements MainDao {
 		list.add(new Video("전신 다이어트 최고의 운동 [칼소폭 찐 핵핵매운맛]", "전신", "gMaB-fG4u4g", "ThankyouBUBU", 0));
 		list.add(new Video("하루 15분! 전신 칼로리 불태우는 다이어트 운동", "전신", "swRNeYw1JkY", "ThankyouBUBU", 0));
 		list.add(new Video("상체 다이어트 최고의 운동 BEST [팔뚝살/겨드랑이살/등살/가슴어깨라인]", "상체", "54tTYO-vU2E", "ThankyouBUBU", 0));
-		list.add(new Video("상체비만 다이어트 최고의 운동 [상체 핵매운맛]", "상체", "QqqZH3j_vH0", "ThankyouBUBU", 0));
-		list.add(new Video("하체운동이 중요한 이유? 이것만 보고 따라하자 ! [하체운동 교과서]", "하체", "tzN6ypk6Sps", "김강민", 0));
+		list.add(new Video("상체비만 다이어트 최고의 운동 [상체 핵매운맛]", "상체", "QqqZH3j_vH0", "ThankyouBUBU", 10));
+		list.add(new Video("하체운동이 중요한 이유? 이것만 보고 따라하자 ! [하체운동 교과서]", "하체", "tzN6ypk6Sps", "김강민", 9));
 		list.add(new Video("저는 하체 식주의자 입니다", "하체", "u5OgcZdNbMo", "GYM종국", 0));
-		list.add(new Video("11자복근 복부 최고의 운동 [복근 핵매운맛]", "복부", "PjGcOP-TQPE", "ThankyouBUBU", 0));
+		list.add(new Video("11자복근 복부 최고의 운동 [복근 핵매운맛]", "복부", "PjGcOP-TQPE", "ThankyouBUBU", 8));
 		list.add(new Video("(Sub)누워서하는 5분 복부운동!! 효과보장! (매일 2주만 해보세요!)", "복부", "7TLk7pscICk", "SomiFit", 0));
 		
 	}
@@ -30,14 +32,34 @@ public class MainDaoImpl implements MainDao {
 
 	@Override
 	public List<Video> selectInterestViewFitVideo() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Video> interestList = new ArrayList<>();
+		Collections.sort(list, new Comparator<Video>() {
+
+			@Override
+			public int compare(Video o1, Video o2) {
+				return o2.getViewCnt()-o1.getViewCnt();
+			} // 내림차순정렬
+		});
+		
+		for (int i=0; i<3; i++) {
+			interestList.add(list.get(i));
+		}
+			
+		return interestList;
 	}
 
 	@Override
 	public List<Video> selectPartfitVideo(String partname) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Video> partList = new ArrayList<>();
+	
+		if (!partname.equals(null)) {
+			for (int i=0; i<list.size(); i++)
+				if (list.get(i).getFitPartName().equals(partname))
+					partList.add(list.get(i));
+			
+		}
+		
+		return partList;
 	}
 
 }
